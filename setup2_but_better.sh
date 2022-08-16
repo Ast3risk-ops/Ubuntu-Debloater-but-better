b77c0a3ce4589900ee8597d64ddffc0c6cb1014f
Ast3risk-ops <spacedude609@pm.me>
2022-08-16 19:45:05 -0400

    Update setup2_but_better.sh
    add missing sudos

--- setup2_but_better.sh
+++ setup2_but_better.sh
@@ -2,8 +2,8 @@
 echo "This script will add pop repos, gnome-tweaks and remove gnome software and the snap store then reboot"
 read -p "THE AUTHOR IS NOT RESPONSIBLE FOR ANY DAMAGE, continue? [y/n] " ANSWER
 if [ $ANSWER == "y" ]; then
-  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 63C46DF0140D738961429F4E204DD8AEC33A7AFF
-  add-apt-repository "deb http://apt.pop-os.org/proprietary $(lsb_release -cs) main"
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 63C46DF0140D738961429F4E204DD8AEC33A7AFF
+  sudo add-apt-repository "deb http://apt.pop-os.org/proprietary $(lsb_release -cs) main"
   sudo apt update
   sudo apt full-upgrade
   sudo apt install gnome-tweaks -y
